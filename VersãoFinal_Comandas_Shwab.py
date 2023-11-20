3d7f54b880a968a412bfb373422c6ca6e1f4dcb4
betotrevisan <138599723+betotrevisan@users.noreply.github.com>
2023-11-20 19:54:37 -0300

    Add files via upload

--- VersãoFinal_Comandas_Shwab.py
+++ VersãoFinal_Comandas_Shwab.py
@@ -1,20 +1,25 @@
 """
 Projeto de controle de consumo dos clientes do recanto SHWAB
 """
-
-from datetime import datetime
-import shutil
 import os
 import pandas as pd
+import numpy as np
+from datetime import datetime
+import shutil
 
-# Criando a comanda
+#Criando a comanda
 
+
+
+
+import os
+import pandas as pd
+import numpy as np
 
 comanda_atual = None
 
-
 def cria_comanda():
-    numero_comanda = input('Digite o número da comanda do cliente: ')
+    numero_comanda = input('Digite o número da comanda a ser liberada ao cliente: ')
     caminho_comanda = os.path.join("Comandas em uso", f"{numero_comanda}.csv")
 
     if os.path.exists(caminho_comanda):
@@ -23,15 +28,30 @@ def cria_comanda():
 
     nova_comanda = pd.DataFrame(columns=['Código', 'Descrição', 'Valor Unitário', 'Quantidade', 'Subtotal'])
 
+
     # Salvando a comanda
     nova_comanda.to_csv(caminho_comanda, index=False)
 
+
+
     print(f'Comanda {numero_comanda} criada com sucesso.')
     print(nova_comanda)
-    return
+
+    resp = input('Deseja incluir um pedido? Tecle y/N: ')
+    if resp == 'y':
+        realiza_compra()
+    else:
+        main()
+
 
 
 # Cliente realiza pedido
+
+import os
+import pandas as pd
+
+import os
+import pandas as pd
 
 def realiza_compra():
     numero_comanda = input('Digite o número da comanda do cliente: ')
@@ -64,26 +84,32 @@ def realiza_compra():
             print('Por gentileza, digite um valor numérico válido')
             continue
 
+        confirma_pedido = input('Confirma pedido? y/N ')
+
+        if confirma_pedido == 'y':
+
         # Adicionando dados na comanda
-        valor_unitario = float(produto['Preço Unitário'].str.replace('R$', '').str.replace(',', '.').values[0])
-        subtotal = valor_unitario * quantidade
+            valor_unitario = float(produto['Preço Unitário'].str.replace('R$', '').str.replace(',', '.').values[0])
+            subtotal = valor_unitario * quantidade
 
-        novo_item = {
-            'Código': int(codigo_produto),
-            'Descrição': produto['Descrição'].values[0],
-            'Valor Unitário': valor_unitario,
-            'QTD': quantidade,
-            'Subtotal': subtotal
-        }
-
-        nova_comanda = nova_comanda._append(novo_item, ignore_index=True)
+            novo_item = {
+                'Código': int(codigo_produto),
+                'Descrição': produto['Descrição'].values[0],
+                'Valor Unitário': valor_unitario,
+                'Quantidade': quantidade,
+                'Subtotal': subtotal
+            }
+            nova_comanda = nova_comanda._append(novo_item, ignore_index=True)
+        else:
+            print('Ok, pedido cancelado! ')
+            main()
 
     # Calculando o 'Total'
     nova_comanda['Total'] = nova_comanda['Subtotal'].sum()
 
     # Formatando os valores com duas casas decimais
     colunas_float = ['Valor Unitário', 'Subtotal', 'Total']
-    nova_comanda[colunas_float] = nova_comanda[colunas_float].map(lambda x: f'{x: .2f}')
+    nova_comanda[colunas_float] = nova_comanda[colunas_float].map(lambda x: f'{x:.2f}')
 
     # Atualizando a comanda
     nova_comanda.to_csv(caminho_comanda, index=False)
@@ -92,7 +118,7 @@ def realiza_compra():
     print(nova_comanda)
 
 
-# Efeutando o pagamento e finalizando a comanda:
+
 def paga_comanda():
     def lista_comandas_em_uso():
         pasta_comandas_em_uso = "Comandas em uso"
@@ -123,7 +149,7 @@ def paga_comanda():
 
     # Exibir a comanda com duas casas decimais
     pd.set_option('display.float_format', '{:.2f}'.format)
-    print(f'Comanda Atual: \n{comanda_atual}')
+    print(f'Comanda Atual:\n{comanda_atual}')
 
     pagamento_confirmado = input("O pagamento foi confirmado? (Digite 'sim' para confirmar): ").lower() == 'sim'
 
@@ -141,14 +167,61 @@ def paga_comanda():
     else:
         print(f'Favor confirmar o pagamento.')
 
+def cancela_pedido():
+
+    numero_comanda = input('Digite o número da comanda do cliente: ')
+    caminho_comanda = os.path.join("Comandas em uso", f"{numero_comanda}.csv")
+
+    if not os.path.exists(caminho_comanda):
+        print(f'A comanda {numero_comanda} não existe.')
+        return
+
+    comanda_atual = pd.read_csv(caminho_comanda)
+
+    print('Comanda Atual:')
+    print(comanda_atual)
+
+    try:
+            indice_pedido = int(input('Digite o índice do pedido a ser cancelado: '))
+    except ValueError:
+        print('Por favor, digite um número válido.')
+        return
+
+    if indice_pedido < 0 or indice_pedido >= len(comanda_atual):
+        print('Índice do pedido inválido.')
+        return
+
+    pedido_cancelado = comanda_atual.iloc[indice_pedido]
+    subtotal_cancelado = float(str(pedido_cancelado['Subtotal']).replace(',', '.'))
+
+    # Removendo o pedido cancelado da comanda
+    comanda_atual = comanda_atual.drop(index=indice_pedido).reset_index(drop=True)
+
+    # Atualizando o 'Total' após cancelar o pedido
+    comanda_atual['Total'] = comanda_atual['Total'] - subtotal_cancelado
+
+    # Formatando os valores com duas casas decimais
+    colunas_float = ['Valor Unitário', 'Subtotal', 'Total']
+    comanda_atual[colunas_float] = comanda_atual[colunas_float].map(lambda x: f'{x:.2f}')
+
+    # Atualizando a comanda
+    comanda_atual.to_csv(caminho_comanda, index=False)
+
+    print(f'Pedido cancelado com sucesso na comanda {numero_comanda}.')
+    print('Comanda Atualizada:')
+    print(comanda_atual)
+
+
+
 
 def main():
     while True:
         print("Escolha uma opção:")
         print("1. Criar comanda")
-        print("2. Realizar compra")
-        print("3. Pagar comanda")
-        print("4. Sair")
+        print("2. Realizar pedido")
+        print("3. Cancelar pedido")
+        print("4. Pagar comanda")
+        print("5. Sair")
 
         opcao = input("Digite o número da opção desejada: ")
 
@@ -157,13 +230,14 @@ def main():
         elif opcao == '2':
             realiza_compra()
         elif opcao == '3':
-            paga_comanda()
+            cancela_pedido()
         elif opcao == '4':
-            print('Saindo da aplicação')
+            paga_comanda()
+        elif opcao == '5':
+            print("Saindo da aplicação")
             break
         else:
             print("Opção inválida. Por favor, escolha uma opção válida.")
-
 
 if __name__ == "__main__":
     main()
